b92e5e05c2838e62f0f28cd49fa5bd4647040f9b
Jacob Buck <buck.jacob@gmail.com>
2018-07-07 11:26:43 +1200

    Rename predicate with comparator to be more in alignment with lodash docs

--- src/index.js
+++ src/index.js
@@ -16,12 +16,12 @@ const takeUniqBy = (iteratee, patternOrChannel, saga, ...args) =>
     }
   });
 
-const takeUniqWith = (predicate, patternOrChannel, saga, ...args) =>
+const takeUniqWith = (comparator, patternOrChannel, saga, ...args) =>
   fork(function*() {
     let actions = [];
     while (true) {
       const action = yield take(patternOrChannel);
-      if (!actions.some(a => predicate(a, action))) {
+      if (!actions.some(a => comparator(a, action))) {
         yield fork(function*() {
           actions = actions.concat([action]);
           yield call(saga, ...args.concat(action));
