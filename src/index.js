49e8a59db2a620aca92019a52beb4dd73c170a40
Jacob Buck <buck.jacob@gmail.com>
2019-02-24 18:29:38 +1300

    Use takeUniqWith in takeUniqBy to avoid duplicated code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import 'regenerator-runtime/runtime';
 import { call, fork, take } from 'redux-saga/effects';
 
-const takeUniqBy = (iteratee, patternOrChannel, saga, ...args) =>
-  fork(function*() {
-    let actions = [];
-    while (true) {
-      const action = yield take(patternOrChannel);
-      if (!actions.some(a => iteratee(a) === iteratee(action))) {
-        yield fork(function*() {
-          actions = actions.concat([action]);
-          yield call(saga, ...args.concat(action));
-          actions = actions.filter(a => a !== action);
-        });
-      }
-    }
-  });
+const takeUniqBy = (iteratee, ...rest) =>
+  takeUniqWith((a, b) => iteratee(a) === iteratee(b), ...rest);
 
 const takeUniqWith = (comparator, patternOrChannel, saga, ...args) =>
   fork(function*() {
